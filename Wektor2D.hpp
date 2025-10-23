1be96e56af845e966cad852bd657adfdb7494b6f
Lukasz <lukaszpogo321@gmail.com>
2025-10-23 17:06:51 +0200

    Created Wektor2D class

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,50 @@
 class Wektor2D
 {
     // Tutaj napisz implementacje klasy Wektor2D
+    public:
+        Wektor2D()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        Wektor2D(double x_wej, double y_wej)
+        {
+            x = x_wej;
+            y = y_wej;
+        }
+
+        Wektor2D operator+(Wektor2D& w) const
+        {
+            return Wektor2D(w.x + x, w.y + y);
+        }
+
+        double operator*(Wektor2D& w) const
+        {
+            return w.x*x + w.y*y;
+        }
+
+        void setX(double x_toset)
+        {
+            x = x_toset;
+        }
+
+        void setY(double y_toset)
+        {
+            y = y_toset;
+        }
+
+        double getX()
+        {
+            return x;
+        }
+
+        double getY()
+        {
+            return y;
+        }
+
+    private:
+        double x;
+        double y;
 };
